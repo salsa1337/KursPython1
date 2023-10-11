522811a37e73fb70d3cab688078a60aa4caa7af4
salsa1337 <147180763+salsa1337@users.noreply.github.com>
2023-10-11 22:31:46 +0300

    Update main1.py

--- Лабораторная работа 1/main1.py
+++ Лабораторная работа 1/main1.py
@@ -1,9 +1,9 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-# TODO заменить значение пропущенного элемента средним арифметическим
-summa1 = sum(numbers[:4:])
-summa2 = sum(numbers[5::])
-dlina = len(numbers)
-numbers[4] = (summa1+summa2)/dlina
+missing_item = 4
+summa = sum(numbers[:missing_item]) + sum(numbers[missing_item+1:])
+count = len(numbers)
+average = summa/count
 
+numbers[missing_item] = average
 print("Измененный список:", numbers)
