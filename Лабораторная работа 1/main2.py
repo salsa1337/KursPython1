5040731282b49c6e85e8784f9f8037800e748646
salsa1337 <147180763+salsa1337@users.noreply.github.com>
2023-10-11 22:36:15 +0300

    Update main2.py

--- Лабораторная работа 1/main2.py
+++ Лабораторная работа 1/main2.py
@@ -1,7 +1,10 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-players = len(list_players)
-kom1 = list_players[:3:]
-kom2 = list_players[3::]
+
+num_players = len(list_players)
+mid = num_players // 2
+
+kom1 = list_players[:mid]
+kom2 = list_players[mid:]
+
 print(kom1)
 print(kom2)
-# TODO Разделите участников на две команды
