991b890ef8eaccfa6b978f72992286ca5e95f3da
salsa1337 <147180763+salsa1337@users.noreply.github.com>
2023-10-15 19:18:06 +0300

    Revert "Лабораторная работа 1"

--- Лабораторная работа 1/main4.py
+++
@@ -1,7 +0,0 @@
-users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
-stats = {"Общее количество": 0, "Уникальные посещения": 0}
-stats["Общее количество"] = len(users)
-unique_users = set(users)
-stats["Уникальные посещения"] = len(unique_users)
-print(stats)
-# TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
